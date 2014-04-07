0c270b5c7662ce9bdcddc62aa3cabd11ffdf8c55
Sang-Hyun <Sang-Hyun@Sang-Hyun-PC>
2014-04-07 23:49:55 +0900

    .

---
+++ src/math/Math12.java
@@ -0,0 +1,34 @@
+package math;
+
+import java.util.Scanner;
+
+public class Math12 {
+	private static Scanner scan = new Scanner(System.in);
+	private static int[] B = new int[4];
+	private static int[] E = new int[4];
+	private static int[] A = new int[4];
+	private static int C = 0;;
+
+	private static void promptCommand(){
+		System.out.println("<<BCD코드와 3초과 코드의 변환>>");
+		System.out.printf("\nBCD 코드 입력 : ");
+		do{
+			String input = scan.nextLine();
+			for(int i=0; i<5; i++){
+				B[i] = input.
+			}
+		}
+
+	}
+
+
+	public static void main(String[] args) {
+		H = new String[NUMBER];
+		T = new String[20];
+		initArray();
+		promptCommand();
+		int i = hexaTrans();
+		hexaShow(i);
+	}
+}
+
