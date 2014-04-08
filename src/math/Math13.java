77c53c9d190a78ddde45efe5274e845257610933
Koo <Koo@s28.java48.com>
2014-04-08 09:12:17 +0900

    .

--- src/math/Math13.java
+++ src/math/Math13.java
@@ -24,47 +24,60 @@ public class Math13 {
 			O = new int[len];
 			T = new int[len];
 			flag = true;
-			for(int i=0; i<len; i++){
-				if(i== len-1)
-					B[i] = Integer.parseInt(input.substring(i));
-				else if(i<len-1 && i >= 0)
-					B[i] = Integer.parseInt(input.substring(i,i+1));
-				else{
-					throw new RuntimeException("Range of index is invalid");
-				}
-				if(B[i] >=0 &&  B[i] <= 1){
-					count++;
-				}else{
-					count = 0;
-					flag = false;
-				}
-			}
-			if(flag==true && count ==len)
+		  int temp = binTrans(len,count,input);
+			if(flag==true && temp ==len)
 				break;
 		}while(true);
 	}
 
+	private static int binTrans(int len, int count, String input){
+		for(int i=0; i<len; i++){
+			if(i== len-1)
+				B[i] = Integer.parseInt(input.substring(i));
+			else if(i<len-1 && i >= 0)
+				B[i] = Integer.parseInt(input.substring(i,i+1));
+			else{
+				throw new RuntimeException("Range of index is invalid");
+			}
+			if(B[i] >=0 &&  B[i] <= 1){
+				count++;
+			}else{
+				count = 0;
+				flag = false;
+			}
+		}
+		return count;
+	}
+	
+	private static void reverse(int i){
+		do{
+			O[i] = 1 - B[i];
+			i++;
+		}while(i < N);	
+	}
+	
+	private static void complement(int i){
+		do{
+			T[i] = 1;
+			if(O[i] == C)
+				T[i] = 0;
+			C = O[i] * C;
+			i = i -1;
+		}while(i>=0);
+	}
+	
 	private static void transform(){
 		int i = 0;
-//		do{
-//			O[i] = 1 - B[i];
-//			i++;
-//		}while(i < N);
-//		i = N;
-//		C = 1;
-//		do{
-//			T[i] = 1;
-//			if(O[i] == C)
-//				T[i] = 0;
-//			C = O[i] * C;
-//			i = i -1;
-//		}while(i>0);
+		reverse(i);
+		i = N-1;
+		C = 1;
+		complement(i);
 	}
-
+	
 	private static void showResult(){
 		System.out.print("2의보수 : ");
 		for(int i=0; i<N; i++){
-			System.out.print(B[i]);
+			System.out.print(T[i]);
 		}
 	}
 
